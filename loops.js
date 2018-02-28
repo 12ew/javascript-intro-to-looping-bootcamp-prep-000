1a96d01b256e765cb0f2eeafe4f9081f7b450ddf
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-28 01:47:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -31,26 +31,10 @@ function whileLoop(number) {
 function doWhileLoop(array) {
  function maybeTrue(){
    return Math.random() >= 0.5
- }
-  
-  array.length > 0 && maybeTrue()
 }
 
 do {
   array.pop();
-  console.log('false');
 } while (array.length > -1 && maybeTrue());
   return array;
 }
-
-
-function doWhileLoop(array) {
-+  function maybeTrue() {
-+    return Math.random() >= 0.5
-+  }
-+
-+  do {
-+    array.pop();
-+  } while (array.length > -1 && maybeTrue());
-+    return array;
-+}
