fff10a74ee8a504383d5fcc20a8ccba51ac12363
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-28 01:46:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -42,3 +42,15 @@ do {
 } while (array.length > -1 && maybeTrue());
   return array;
 }
+
+
+function doWhileLoop(array) {
++  function maybeTrue() {
++    return Math.random() >= 0.5
++  }
++
++  do {
++    array.pop();
++  } while (array.length > -1 && maybeTrue());
++    return array;
++}
